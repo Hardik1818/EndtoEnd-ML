9d19086d034dc91a116db26a84e8223014777906
Hardik1818 <thapaliyahardik1818@gmail.com>
2026-01-06 17:53:16 +0545

    created file structure with components,pipeline,logger,expection.py

---
+++ src/logger.py
@@ -0,0 +1,24 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
+
+LOG_FILE = f"{datetime.now().strftime('%Y_%m_%d_%H_%M_%S')}.log"
+LOG_PATH = os.path.join(LOG_DIR, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_PATH,
+    format="[%(asctime)s] %(levelname)s - %(message)s",
+    level=logging.INFO
+)
+
+def get_logger():
+    return logging.getLogger()
+
+
+# if __name__ == "__main__":
+#     logger = get_logger()
+#     logger.info("Logger has been set up.")
+#     logger.error("This is a test error message.")
